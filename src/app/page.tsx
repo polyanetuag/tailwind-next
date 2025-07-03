899162190e04de299e6884874145ec8612d00b89
Polyane Tuag <pollyportugues@gmail.com>
2025-07-03 19:55:21 -0300

    Enhance Home component layout with flexbox for improved structure and readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
 export default function Home() {
   return (
-    <div className="bg-gray-900 text-slate-200 h-screen p-8">
-      <h1 className="text-4xl text-amber-400 font-bold">Hello Tailwind</h1>
+    <div className=" bg-gray-900 text-slate-200 h-screen p-8 ">
+      <h1 className="flex items-center text-4xl text-amber-400 font-bold before:w-0.5 before:h-4 before:bg-amber-100 gap-2 before:rounded-md ">
+        Hello Tailwind
+      </h1>
       <p>Aprendendo a utilizar a ferramenta</p>
+
+      <button
+        // disabled
+        className="bg-gray-600 px-2 py-1 mt-4 rounded-md font-medium cursor-pointer transition-colors enabled:hover:bg-gray-500 disabled:opacity-50 disabled:cursor-not-allowed"
+      >
+        Sign In
+      </button>
     </div>
   );
 }
