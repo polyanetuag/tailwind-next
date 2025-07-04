35123cfcb06d7d761e3f71cd53fc1dd7a9ce0fdf
Polyane Tuag <pollyportugues@gmail.com>
2025-07-04 17:56:27 -0300

    Update Home component styles for improved accessibility and visual consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className=" bg-gray-900 text-slate-200 h-screen p-8 ">
+    <div className=" bg-amber-50 text-gray-900 h-screen p-8 dark:bg-gray-900 dark:text-gray-100 ">
       <h1 className="flex items-center text-3xl sm:text-4xl md:text-5xl text-amber-400 font-bold before:w-0.5 before:h-4 before:bg-amber-100 gap-2 before:rounded-md ">
         Hello Tailwind
       </h1>
@@ -8,7 +8,7 @@ export default function Home() {
 
       <button
         // disabled
-        className="bg-gray-600 px-2 py-1 mt-4 rounded-md font-medium cursor-pointer transition-colors enabled:hover:bg-gray-500 disabled:opacity-50 disabled:cursor-not-allowed"
+        className="bg-gray-600 text-white px-2 py-1 mt-4 rounded-md font-medium cursor-pointer transition-colors enabled:hover:bg-gray-500 disabled:opacity-50 disabled:cursor-not-allowed"
       >
         Sign In
       </button>
