bd84bde8f79cda9f7ad4ab9dc2da1088f53ceb18
Polyane Tuag <pollyportugues@gmail.com>
2025-07-05 16:01:12 -0300

    chore: add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import("tailwindcss").Config} */
+module.exports = {
+    content: ["./src/***.tsx"],
+    theme: {
+        extend: {
+            gridTemplateColumns: {
+                app: "minmax(18rem, 20rem) 1fr"
+            }
+        }
+    }
+}
