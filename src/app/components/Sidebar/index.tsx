7d4cca11da33a137461f737d690915194039758b
Polyane Tuag <pollyportugues@gmail.com>
2025-07-09 19:31:22 -0300

    feat: replace Input component

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -2,13 +2,19 @@ import { Logo } from "./Logo";
 import { MainNavigation } from "./MainNavigation";
 import { UsedSpaceWidget } from "./UsedSpaceWidget";
 import { Profile } from "./Profile";
-import { Input } from "./Input";
+import { InputControl, InputPrefix, InputRoot } from "./Input";
+import { Search } from "lucide-react";
 
 export function Sidebar() {
   return (
     <aside className="border-r border-zinc-200 px-5 py-8 shadow flex flex-col gap-6">
       <Logo />
-      <Input />
+      <InputRoot>
+        <InputPrefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </InputPrefix>
+        <InputControl placeholder="Search" />
+      </InputRoot>
 
       <MainNavigation />
 
