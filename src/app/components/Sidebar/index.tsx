a0c5503a0c38b6fc91a44b1c07113984b823f6ba
Polyane Tuag <pollyportugues@gmail.com>
2025-07-09 19:17:39 -0300

    feat: add Input component to Sidebar for search functionality

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,29 +1,21 @@
-import { Cog, LifeBuoy, Search } from "lucide-react";
 import { Logo } from "./Logo";
 import { MainNavigation } from "./MainNavigation";
-import { NavItem } from "./MainNavigation/NavItem";
 import { UsedSpaceWidget } from "./UsedSpaceWidget";
 import { Profile } from "./Profile";
+import { Input } from "./Input";
 
 export function Sidebar() {
-    return (
-        <aside className="border-r border-zinc-200 px-5 py-8 shadow flex flex-col gap-6">
-        <Logo />   
-        <div className="flex mx-1 w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm ">
-            <Search className="h-5 w-5 text-zinc-500" />
-            <input className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600" placeholder="Search" />
-        </div>
+  return (
+    <aside className="border-r border-zinc-200 px-5 py-8 shadow flex flex-col gap-6">
+      <Logo />
+      <Input />
 
-        <MainNavigation />
+      <MainNavigation />
 
-        <UsedSpaceWidget />
+      <UsedSpaceWidget />
 
-        <div className="h-px bg-zinc-200"/>
-            <Profile />
-        
-        </aside>
-    ) 
-    
-    
-   
+      <div className="h-px bg-zinc-200" />
+      <Profile />
+    </aside>
+  );
 }
